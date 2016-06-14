082461e7c42221cb8950ef0f490c09fdd08a7b00
KwonSeungHyeop <suhy2002@naver.com>
2016-06-14 11:00:25 +0900

    s

---
+++ codingM/src/main/webapp/songs/song.js
@@ -0,0 +1,29 @@
+loadDate();
+function loadDate() {
+  $.getJSON("data.json", function(result) {
+    var templateData = $('#temp1').html()
+    var template = Handlebars.compile(templateData)
+    var html = template(result)
+    $(".page").append(html)
+  });
+}
+
+function getImage() {
+  var newdiv = document.createElement("div");
+  var add = $(".sect1").clone();
+  $(newdiv).html(add);
+  jQuery(".page").append(newdiv);
+}
+jQuery(document).ready(function() {
+
+  getImage();
+
+  //scroll : 스크롤 사용시 발생되는 이벤트
+  jQuery(window).scroll(function(){
+    var docH=$(document).height();
+    var scrollH = jQuery(window).height() + jQuery(window).scrollTop();
+    if(scrollH>=docH) {
+      getImage();
+    }
+  });
+});
