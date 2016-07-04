8d60103275951cd8a3cb7683df51ef54575e3f67
KwonSeungHyeop <suhy2002@naver.com>
2016-07-04 16:56:19 +0900

    song monday 17:00

--- codingM/src/main/webapp/songs/song.js
+++ codingM/src/main/webapp/songs/song.js
@@ -6,6 +6,13 @@ function imageWall() {
   });
 }
 
+Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+  if(v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
 window.onload = function loadDate() {
   $.getJSON("http://localhost:8080/codingM/songs/list.do", function(result) {
     var templateData = $('#temp1').html()
@@ -16,23 +23,23 @@ window.onload = function loadDate() {
   });
 }
 
-function getImage() {
+
+
+function getSong() {
   var newdiv = document.createElement("div");
   var add = $(".demo").clone();
   $(newdiv).html(add);
   jQuery(".viewport").append(newdiv);
-
 }
-jQuery(document).ready(function() {
 
-  getImage();
+jQuery(document).ready(function() {
 
   //scroll : 스크롤 사용시 발생되는 이벤트
   jQuery(window).scroll(function(){
     var docH=$(document).height();
     var scrollH = jQuery(window).height() + jQuery(window).scrollTop();
     if(scrollH>=docH) {
-      getImage();
+      getSong();
     }
   });
 });
